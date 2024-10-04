b0951500b244f56a2c856ad238036f422a1034ac
laura13usero <164626381+laura13usero@users.noreply.github.com>
2024-10-04 12:17:09 +0200

    Add files via upload

---
+++ Simpson/src/Simpson.java
@@ -0,0 +1,25 @@
+
+public class Simpson {
+
+    //VISIBLE A NIVEL DE LA CLASE SIMPSON
+    char[][] tablero = new char[10][10];
+    //Software gestion de venta ONLINE
+    public static void rellenarTablero(char pers){
+    for(int i = o; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            tablero[0][0] = 'L'
+        }
+    }
+    }
+    public static void main(String[] args) {
+
+        System.out.printf("Hello and welcome!");
+                                                                                      //int  contador = 0;
+        char personaje = '#';
+        rellenarTablero(personaje);
+                                                                                        // contador = 10;
+                                                                                        //rellenarTablero(contador);
+
+
+    }
+}
